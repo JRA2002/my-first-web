a69d9a51f2a8252add49850a053e4a639a043437
JRA2002 <105953543+JRA2002@users.noreply.github.com>
2024-04-08 18:11:00 +0200

    f

--- webpage/library_chile/settings.py
+++ webpage/library_chile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%*ygno%#v(agyacy0)tdk6vf+9)7n4#s%4$ga%#$$^az(&#&-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
